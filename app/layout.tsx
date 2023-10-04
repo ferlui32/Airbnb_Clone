80b5e2920ca1380ab73c7d6622487ba20de25ea6
ferlui32 <luizferri@gmail.com>
2023-10-04 10:44:28 +0100

    filters modal

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import RegisterModal from './components/modals/RegisterModal'
 import RentModal from './components/modals/RentModal'
 import ToasterProvider from './providers/ToasterProvider'
 import getCurrentUser from './actions/getCurrentUser'
+import SearchModal from './components/modals/SearchModal'
 
 
 const font = Nunito({ subsets: ['latin'] })
@@ -25,6 +26,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <SearchModal />
         <RentModal />
         <LoginModal />
         <RegisterModal />
