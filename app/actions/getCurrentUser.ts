25d976999a769eab2b5973ef9bb5db0fba248abb
ferlui32 <luizferri@gmail.com>
2023-10-04 15:53:28 +0100

    fixed error on  getCurrentUser.ts

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -12,7 +12,7 @@ export default async function getCurrentUser() {
     try {
         const session = await getSession()
 
-        if (!session?.user.email) {
+        if (!session?.user?.email) {
             return null
         }
 
